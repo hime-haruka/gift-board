d1b7403e05ccf0853c2afd0b9b082e4a584875c5
hime-haruka <hime-haruka@naver.com>
2026-04-26 16:17:21 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,2 +1,31 @@
-// placeholder server
-console.log('server running');
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+// 테스트용
+app.get("/", (req, res) => {
+  res.send("Server is alive");
+});
+
+// overlay
+app.get("/overlay", (req, res) => {
+  res.sendFile(__dirname + "/overlay/index.html");
+});
+
+// admin
+app.get("/admin", (req, res) => {
+  res.sendFile(__dirname + "/admin/index.html");
+});
+
+// 이벤트 수신
+app.post("/api/event", (req, res) => {
+  console.log("EVENT:", req.body);
+  res.sendStatus(200);
+});
+
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
